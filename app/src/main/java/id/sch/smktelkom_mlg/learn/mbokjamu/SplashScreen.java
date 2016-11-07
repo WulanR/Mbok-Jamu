62d0591549eae7d8bdcf3837e31fdcb1e0ac0477
Wulan Rohmadani <wulanr161299@gmail.com>
2016-11-07 21:31:14 +0700

    Initial commit Splashscreen

--- app/src/main/java/id/sch/smktelkom_mlg/learn/mbokjamu/SplashScreen.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/mbokjamu/SplashScreen.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.learn.mbokjamu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -18,6 +19,21 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.Layout.SplashScreen);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                this.finish();
+            }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
     }
 }
